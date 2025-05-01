05502cefc294fcd4d2a957f92d1d140e6c40f5cb
bernabarros <bernardobarros.work@gmail.com>
2025-05-01 11:18:12 +0100

    Create project PlayerManager2

---
+++ PlayerManager2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerManager2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
